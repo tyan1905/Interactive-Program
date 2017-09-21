92cb7e62bf77c6f7ee610caffb61109fb6bd63f7
tyan1905 <tyan1905@mpsedu.org>
2017-09-21 09:38:35 -0500

    Update DIE NOT DIE.py

--- CSP-PYTHONPRJCT1/DIE NOT DIE.py
+++ CSP-PYTHONPRJCT1/DIE NOT DIE.py
@@ -1,7 +1,31 @@
+#roll 1 = 1 die
+#roll 2 = 2 dices
+#roll 3 = 3 dices 
+#roll 4 = 4 dices
+#roll 5 = you get the point
 import random as rnd
 while(True):
     input = str.lower(raw_input("Type Roll"))
-    if("roll" in input):
-        print rnd.randint(1,20)
+if("roll5" in input):
+      print rnd.randint(1,6)  
+      print rnd.randint(1,6)
+      print rnd.randint(1,6)  
+      print rnd.randint(1,6)
+      print rnd.randint(1,6)  
+elif("roll4" in input):
+      print rnd.randint(1,6)  
+      print rnd.randint(1,6)
+      print rnd.randint(1,6)
+      print rnd.randint(1,6)  
+elif("roll3" in input):
+      print rnd.randint(1,6)  
+      print rnd.randint(1,6)
+      print rnd.randint(1,6)  
+elif("roll2" in input):
+      print rnd.randint(1,6)  
+      print rnd.randint(1,6)
+elif("roll1" in input):
+      print rnd.randint(1,6)
     else:
         print "Please Type Roll"
+        
